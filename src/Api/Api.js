12f13b510bd76937e5d4492ce7d074ac0e8990a2
IrinaAginskaya <aginska@gmail.com>
2022-01-12 22:48:31 +0200

    add modal_Loader_Item_api

---
+++ src/Api/Api.js
@@ -0,0 +1,17 @@
+function fetchImage(value, page) {
+  const KEY = '24233847-0df8e18c0b7a82668deba14ff';
+  return fetch(
+    `https://pixabay.com/api/?q=${value}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    return Promise.reject(new Error(`there is no image here`));
+  });
+}
+const api = {
+  fetchImage,
+};
+
+export default api;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,8 @@
+function Button({ onLoadMore }) {
+  return (
+    <button type="button" onClick={onLoadMore}>
+      Load more
+    </button>
+  );
+}
+export default Button;
